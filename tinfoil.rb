8e5a2020b6149fc8d0c702819d7acd42be9ae87f
= <pjfritch1@gmail.com>
2017-11-29 15:10:13 -0800

    update treefunk

--- tinfoil.rb
+++ tinfoil.rb
@@ -70,60 +70,60 @@ l2 | _    _ | r2
 
 
 Class Intepreter
-def initalize
-  @tree = InfiniteTree.new
-  @current_node = InfiniteTree[0]
-  @instruction_pointer = 0
-  @str = ""
-end
-
-
-def eval(str)
-  @str = str
-  until @instruction_pointer >= str.length
-    handle_input(str[@instruction_pointer])
-    @instruction_pointer += 1
+  def initalize
+    @tree = InfiniteTree.new
+    @current_node = InfiniteTree[0]
+    @instruction_pointer = 0
+    @str = ""
   end
-end
 
-def handle_input(input)
-  if input == "<"
-    less_than
-  elsif input == ">"
-    greater_than
-  elsif input == "["
-    left_bracket
-  elsif input == "]"
-    right_bracket
-  #...
+
+  def eval(str)
+    @str = str
+    until @instruction_pointer >= str.length
+      handle_input(str[@instruction_pointer])
+      @instruction_pointer += 1
+    end
   end
-end
 
-def less_than
-  @current_node = @current_node.left
-end
+  def handle_input(input)
+    if input == "<"
+      less_than
+    elsif input == ">"
+      greater_than
+    elsif input == "["
+      left_bracket
+    elsif input == "]"
+      right_bracket
+    #...
+    end
+  end
 
-def left_bracket
-  if @current_node.value == 0
-    left_counter = 0
-    right_counter = 0
-    @str[@instruction_pointer + 1..-1].each_char.with_index do |chr, i|
-      if chr == "["
-        left_counter += 1
-      elsif chr == "]"
-        if left_counter == right_counter
-          @instruction_pointer = i
-          return
-        else
-          right_counter += 1
+  def less_than
+    @current_node = @current_node.left
+  end
+
+  def left_bracket
+    if @current_node.value == 0
+      left_counter = 0
+      right_counter = 0
+      @str[@instruction_pointer + 1..-1].each_char.with_index do |chr, i|
+        if chr == "["
+          left_counter += 1
+        elsif chr == "]"
+          if left_counter == right_counter
+            @instruction_pointer = i
+            return
+          else
+            right_counter += 1
+          end
         end
       end
     end
   end
-end
 
-def right_bracket
-end
+  def right_bracket
+  end
 
 end
 
